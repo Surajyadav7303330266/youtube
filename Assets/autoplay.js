56b9df36719a2f0a8562193ff33e79b9ed72b605
Surajyadav7303330266 <sy9621551@gmail.com>
2019-12-19 16:31:25 +0530

    ten

---
+++ Assets/autoplay.js
@@ -0,0 +1,181 @@
+var $video1 = $('#one');
+
+$video1.on('mouseover focus', function () {
+  $video1.get(0).play();
+});
+
+$video1.on('mouseout blur', function () {
+  $video1.get(0).pause();
+});
+var $video2 = $('#two');
+
+$video2.on('mouseover focus', function () {
+  $video2.get(0).play();
+});
+
+$video2.on('mouseout blur', function () {
+  $video2.get(0).pause();
+});
+var $video3 = $('#three');
+
+$video3.on('mouseover focus', function () {
+  $video3.get(0).play();
+});
+
+$video3.on('mouseout blur', function () {
+  $video3.get(0).pause();
+});
+var $video4 = $('#four');
+
+$video4.on('mouseover focus', function () {
+  $video4.get(0).play();
+});
+
+$video4.on('mouseout blur', function () {
+  $video4.get(0).pause();
+});
+var $video5 = $('#five');
+
+$video5.on('mouseover focus', function () {
+  $video5.get(0).play();
+});
+
+$video5.on('mouseout blur', function () {
+  $video5.get(0).pause();
+});
+var $video6 = $('#six');
+
+$video6.on('mouseover focus', function () {
+  $video6.get(0).play();
+});
+
+$video6.on('mouseout blur', function () {
+  $video6.get(0).pause();
+});
+var $video7 = $('#seven');
+
+$video7.on('mouseover focus', function () {
+  $video7.get(0).play();
+});
+
+$video7.on('mouseout blur', function () {
+  $video7.get(0).pause();
+});
+var $video8 = $('#eight');
+
+$video8.on('mouseover focus', function () {
+  $video8.get(0).play();
+});
+
+$video8.on('mouseout blur', function () {
+  $video8.get(0).pause();
+});
+var $video9 = $('#nine');
+
+$video9.on('mouseover focus', function () {
+  $video9.get(0).play();
+});
+
+$video9.on('mouseout blur', function () {
+  $video9.get(0).pause();
+});
+var $video10 = $('#ten');
+
+$video10.on('mouseover focus', function () {
+  $video10.get(0).play();
+});
+
+$video10.on('mouseout blur', function () {
+  $video10.get(0).pause();
+});
+//kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+var $video11 = $('#eleven');
+
+$video11.on('mouseover focus', function () {
+  $video11.get(0).play();
+});
+
+$video11.on('mouseout blur', function () {
+  $video11.get(0).pause();
+});
+var $video12 = $('#twelve');
+
+$video12.on('mouseover focus', function () {
+  $video12.get(0).play();
+});
+
+$video12.on('mouseout blur', function () {
+  $video12.get(0).pause();
+});
+var $video13 = $('#thirteen');
+
+$video13.on('mouseover focus', function () {
+  $video13.get(0).play();
+});
+
+$video13.on('mouseout blur', function () {
+  $video13.get(0).pause();
+});
+var $video14 = $('#fourteen');
+
+$video14.on('mouseover focus', function () {
+  $video14.get(0).play();
+});
+
+$video14.on('mouseout blur', function () {
+  $video14.get(0).pause();
+});
+var $video15 = $('#fifteen');
+
+$video15.on('mouseover focus', function () {
+  $video15.get(0).play();
+});
+
+$video15.on('mouseout blur', function () {
+  $video15.get(0).pause();
+});
+var $video16 = $('#sixteen');
+
+$video16.on('mouseover focus', function () {
+  $video16.get(0).play();
+});
+
+$video16.on('mouseout blur', function () {
+  $video16.get(0).pause();
+});
+var $video17 = $('#seventeen');
+
+$video17.on('mouseover focus', function () {
+  $video17.get(0).play();
+});
+
+$video17.on('mouseout blur', function () {
+  $video17.get(0).pause();
+});
+var $video18 = $('#eighteen');
+
+$video18.on('mouseover focus', function () {
+  $video18.get(0).play();
+});
+
+$video18.on('mouseout blur', function () {
+  $video18.get(0).pause();
+});
+var $video19 = $('#nineteen');
+
+$video19.on('mouseover focus', function () {
+  $video19.get(0).play();
+});
+
+$video19.on('mouseout blur', function () {
+  $video19.get(0).pause();
+});
+var $video20 = $('#twenty');
+
+$video20.on('mouseover focus', function () {
+  $video20.get(0).play();
+});
+
+$video20.on('mouseout blur', function () {
+  $video20.get(0).pause();
+});
---
+++ Assets/playOnHover.js
@@ -0,0 +1,4 @@
+var load=document.getElementById('playOnHover');
+load.on('mauseenter',function(){
+	load.get(0).play();
+});
